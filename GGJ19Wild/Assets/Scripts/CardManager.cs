ac5e975eb19edf44539f2f0b4d2f16b1617cbcd2
Maicon Silva Pinto de Abreu Santana <maiconspas@gmail.com>
2022-07-23 12:34:11 +0100

    move code to correct place and hide card already used

--- GGJ19Wild/Assets/Scripts/CardManager.cs
+++ GGJ19Wild/Assets/Scripts/CardManager.cs
@@ -1,33 +1,49 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class CardManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
  {
-     private bool mouse_over = false;
-     private RectTransform rt;
+    private bool mouse_over = false;
+    public CardModel cardModel;
+    private RectTransform rt;
+    private Image image;
+    public TopDownController playerController;
 
-     void Awake()
-     {
-        
-            rt = GetComponent (typeof (RectTransform)) as RectTransform;
-     }
+    void Awake()
+    {
+        rt = GetComponent (typeof (RectTransform)) as RectTransform;
+        rt.sizeDelta = new Vector2 (100, 100);
+        Button newButton = GetComponent<Button>();
+        newButton.onClick.AddListener(delegate{ TaskOnClick(); });
+        image = GetComponent (typeof (Image)) as Image;
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
  
-     void Update()
-     {
-         if (mouse_over)
-         {
+    void Update()
+    {
+        this.image.sprite = this.cardModel.sprite;
+        if (mouse_over)
+        {
             rt.sizeDelta = new Vector2 (200, 200);
-         }else{
+        }else{
             rt.sizeDelta = new Vector2 (100, 100);
-         }
-     }
-     public void OnPointerEnter(PointerEventData eventData)
-     {
-         mouse_over = true;
-     }
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        mouse_over = true;
+    }
  
-     public void OnPointerExit(PointerEventData eventData)
-     {
-         mouse_over = false;
-     }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        mouse_over = false;
+    }
+    void TaskOnClick(){
+        this.playerController.UseCurrentCard(this.cardModel);
+        this.enabled = false;
+        rt.sizeDelta = new Vector2 (0, 0);
+    }
  }
--- GGJ19Wild/Assets/Scripts/HandCardManager.cs
+++ GGJ19Wild/Assets/Scripts/HandCardManager.cs
@@ -21,21 +21,11 @@ public class HandCardManager : MonoBehaviour, IPointerEnterHandler, IPointerExit
             var tempCard = playerController.AllCards[i];
             GameObject newButton = DefaultControls.CreateButton(new DefaultControls.Resources());
             newButton.AddComponent(typeof(CardManager));
+            newButton.GetComponent<CardManager>().cardModel = tempCard;
+            newButton.GetComponent<CardManager>().playerController = playerController;
             newButton.transform.SetParent(transform, false);
-            RectTransform rt = newButton.GetComponent (typeof (RectTransform)) as RectTransform;
-            rt.sizeDelta = new Vector2 (100, 100);
-            Button newButton2 = newButton.GetComponent<Button>();
-            newButton2.onClick.AddListener(delegate{ TaskOnClick(tempCard); });
-            var spriteRenderer = newButton.GetComponent<Image>();
-            spriteRenderer.sprite = tempCard.sprite;
-            foreach (Transform child in newButton.transform) {
-                GameObject.Destroy(child.gameObject);
-            }
         }
     }
-	void TaskOnClick(CardModel card){
-        this.playerController.UseCurrentCard(card);
-	}
 
 
     // Update is called once per frame
